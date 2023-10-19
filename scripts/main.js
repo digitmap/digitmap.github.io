9139543cb34f504a8fa53c1484886a59e0f4c9c6
Gaius <ndigitmap@gmail.com>
2023-10-19 20:33:30 +0800

    m3,np,woody's ball

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,116 @@
-const myImage = document.querySelector("img");
 
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
-  } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
-  }
-};
+  // 增加随机碰撞背景
+  // setup canvas
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-function setUserName() {
-	const myName = prompt("Please enter your name.");
-	if (!myName) {
-	  setUserName();
-	} else {
-	  localStorage.setItem("name", myName);
-	  myHeading.textContent = `Mozilla is cool, ${myName}`;
-	}
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+class Ball {
+  static defaultMinSize = 10;
+  static defaultMaxSize = 30;
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
   }
   
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0 * Math.PI, 2 * Math.PI);
+    ctx.fill();
+  }
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
   
-  if (!localStorage.getItem("name")) {
-	setUserName();
-  } else {
-	const storedName = localStorage.getItem("name");
-	myHeading.textContent = `Mozilla is cool, ${storedName}`;
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+          ball.size = random(Ball.defaultMinSize,Ball.defaultMaxSize);
+          this.size = random(Ball.defaultMinSize,Ball.defaultMaxSize);
+          ball.velX = random(-2,2);
+          ball.velY = random(-2,2);
+          this.velX = random(-2,2);
+          this.velY = random(-2,2);
+        }
+      }
+    }
   }
   
-  myButton.onclick = () => {
-	setUserName();
-  };
-  
+}
+
+const balls = [];
+
+while (balls.length < 16) {
+  const size = random(Ball.defaultMinSize,Ball.defaultMaxSize);
+
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-2, 2),
+    random(-2, 2),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 1)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+
+loop();
